7d120966eacffb3d1ae4492fc2a08f6b9bb540f8
Venkata Kakumani <venkata@govtechpros.com>
2024-04-07 00:47:13 -0400

    after some updates in some of the files

---
+++ frontend/src/Login.js
@@ -0,0 +1,43 @@
+// "rfce" : a shortcut to get the react functional component with ES7 module system
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login(){
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+    return(
+        <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+            <div className="bg-white p-3 rounded w-25">
+            <h2>Sign-Up</h2>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder="Email" name="email"
+                        onChange = {handleInput} className="form-control rounded-0"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="Password" name="passwird"
+                        onChange = {handleInput} className="form-control rounded-0"/>
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">Log in</button>
+                    <p>You are agreed to our terms and conditions</p>
+                    <Link to="/signup" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">Create Account</Link>
+
+                </form>
+            </div>
+            <p>You are agreed to our terms and conditions</p>
+        </div>
+    )
+}
+
+export default Login
